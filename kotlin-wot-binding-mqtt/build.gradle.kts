d9ae2f07f5ccced6d45965a0f22bffe846851d4b
Robert Winkler <rwinkler@telekom.de>
2024-11-12 13:23:51 +0100

    MQTT progress

---
+++ kotlin-wot-binding-mqtt/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    api(project(":kotlin-wot"))
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    implementation("com.hivemq:hivemq-mqtt-client:1.3.3")
+    testImplementation("ch.qos.logback:logback-classic:1.5.12")
+    testImplementation("app.cash.turbine:turbine:1.2.0")
+    testImplementation("org.testcontainers:testcontainers:1.20.3")
+}
