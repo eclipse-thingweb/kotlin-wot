e10a09236fced9569c8d828cd65806f92d2f54d8
Robert Winkler <robwin@t-online.de>
2025-04-27 11:32:23 +0200

    Fix dependencies

--- kotlin-wot/build.gradle.kts
+++ kotlin-wot/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies {
-    //implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.20")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     api("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.3")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.3")
 
